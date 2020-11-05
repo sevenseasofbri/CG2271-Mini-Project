f8892c45ef30547d8eb121d792e511a4c83e0984
sevenseasofbri <vishruti250102@gmail.com>
2020-11-05 23:08:28 +0800

    Add audio to rtos

--- RTOS_Project/myInit.c
+++ RTOS_Project/myInit.c
@@ -6,9 +6,11 @@
 /* INIT CLOCK GATING */
 void initClockGate() {
 	// Enable Clock Gating for PORTB and PORTE
-	SIM->SCGC5 |= (SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTE_MASK) | (SIM_SCGC5_PORTA_MASK);
+	SIM->SCGC5 |= (SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTE_MASK) | (SIM_SCGC5_PORTA_MASK) | (SIM_SCGC5_PORTD_MASK);
 	// Enable Clock Gating for UART2
 	SIM->SCGC4 |= SIM_SCGC4_UART2_MASK;
+		//Enable clock gating for Timer0, Timer 1 and Timer 2
+	SIM->SCGC6 = (SIM_SCGC6_TPM0_MASK) | (SIM_SCGC6_TPM1_MASK) | (SIM_SCGC6_TPM2_MASK);
 }
 
 
@@ -24,9 +26,6 @@ void initPWM() {
 	PORTB->PCR[RIGHT_FW] |= PORT_PCR_MUX(3);
 	PORTB->PCR[RIGHT_BK] &= ~PORT_PCR_MUX_MASK;
 	PORTB->PCR[RIGHT_BK] |= PORT_PCR_MUX(3);
-	
-	//Enable clock gating for Timer1
-	SIM->SCGC6 = (SIM_SCGC6_TPM1_MASK)|(SIM_SCGC6_TPM2_MASK);
 	
 	//Select clock for TPM module
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
@@ -121,3 +120,34 @@ void initLED(void) {
  PTB->PDDR |= (MASK(LED_F8) | MASK(LED_F9) | MASK(LED_F10) | MASK(LED_F11));
  PTE->PDDR |= (MASK(LED_F2) | MASK(LED_F3) | MASK(LED_F4) | MASK(LED_F5));
 }
+
+void initAudio() {
+
+	// Configure Mode 3 for PWM pin operation
+	PORTD->PCR[PTD0_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[PTD0_Pin] |= PORT_PCR_MUX(4);
+	PORTD->PCR[PTD1_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[PTD1_Pin] |= PORT_PCR_MUX(4);
+	
+	//Select clock for TPM module
+	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
+	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); //MCGFLLCLK OR MCGPLLCLK/2
+	
+	//set modulo value 48000000/128 = 375000, 375000Hz/50Hz = 7500	
+	TPM0->MOD = 7500;
+	
+	//Edge-Aligned PWM
+	//CMOD - 1 and PS - 111 (128)
+	TPM0_SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM0_SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7)); //CMOD = 1 => LPTPM counter increments on every LPTPM counter clock
+	TPM0_SC &= ~(TPM_SC_CPWMS_MASK); //count up by default (0)
+
+	//enable PWM on TPM0 channel 0 - PTD0
+	TPM0_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//enable PWM on TPM0 channel 1 - PTD1
+	TPM0_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+}
+
--- RTOS_Project/mySystem.c
+++ RTOS_Project/mySystem.c
@@ -6,14 +6,12 @@
 
 #define MSG_COUNT 1
 
-
-
 // Define commands for led rear control
 #define LED_RED_MOVE 0x01
 #define LED_RED_STOP 0x02
 
 volatile int speed = 2;
-uint8_t rx_data = 0x01;
+uint8_t rx_data = 0x00;
 uint8_t uart_data;
 
 unsigned int counter = 0;
@@ -27,6 +25,14 @@ typedef enum colours {
  led_a = LED_R1, 
 } colours_t;
 
+typedef enum state {
+	FORWARD,
+	REVERSE,
+	RIGHT,
+	LEFT,
+	STOP
+} state_t;
+
 typedef enum onOff {
  on,
  off
@@ -34,9 +40,99 @@ typedef enum onOff {
 
 char led_colours[2][2] = {{0, led_a}};
 
-	
+// change this to make the song slower or faster
+int tempo = 144;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int hedwigsThemeMelody[] = {
+
+
+  // Hedwig's theme fromn the Harry Potter Movies
+  // Socre from https://musescore.com/user/3811306/scores/4906610
+  
+  REST, 2, NOTE_D4, 4,
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4,
+  NOTE_G4, 2, NOTE_D5, 4,
+  NOTE_C5, -2, 
+  NOTE_A4, -2,
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4,
+  NOTE_F4, 2, NOTE_GS4, 4,
+  NOTE_D4, -1, 
+  NOTE_D4, 4,
+
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4, //10
+  NOTE_G4, 2, NOTE_D5, 4,
+  NOTE_F5, 2, NOTE_E5, 4,
+  NOTE_DS5, 2, NOTE_B4, 4,
+  NOTE_DS5, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_B4, 4,
+  NOTE_G4, -1,
+  NOTE_AS4, 4,
+     
+  NOTE_D5, 2, NOTE_AS4, 4,//18
+  NOTE_D5, 2, NOTE_AS4, 4,
+  NOTE_DS5, 2, NOTE_D5, 4,
+  NOTE_CS5, 2, NOTE_A4, 4,
+  NOTE_AS4, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_D4, 4,
+  NOTE_D5, -1, 
+  REST,4, NOTE_AS4,4,  
+
+  NOTE_D5, 2, NOTE_AS4, 4,//26
+  NOTE_D5, 2, NOTE_AS4, 4,
+  NOTE_F5, 2, NOTE_E5, 4,
+  NOTE_DS5, 2, NOTE_B4, 4,
+  NOTE_DS5, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_AS4, 4,
+  NOTE_G4, -1, 
+  
+};
+
+int starWarsMelody [] = {
+	NOTE_A4, 500, NOTE_A4, 500, NOTE_A4, 500, NOTE_A4, 350, NOTE_C5, 150, NOTE_A4, 500, NOTE_F4, 350, 
+	NOTE_C5, 150, NOTE_A4, 650, REST, 500, NOTE_E5, 500, NOTE_E5, 500, NOTE_E5, 500, NOTE_F5, 350, 
+	NOTE_C5, 150, NOTE_GS4, 500, NOTE_F4, 350, NOTE_C5, 150, NOTE_A4, 650, REST, 500, NOTE_A5, 500, 
+	NOTE_A4, 300, NOTE_A4, 150, NOTE_A5, 500, NOTE_GS5, 325, NOTE_G5, 175, NOTE_FS5, 125, NOTE_F5, 125, 
+	NOTE_FS5, 250, REST, 325, NOTE_AS, 250, NOTE_DS5, 500, NOTE_D5, 325, NOTE_CS5, 175, NOTE_C5, 125, 
+	NOTE_AS4, 125, NOTE_C5, 250, REST, 350, NOTE_F4, 250, NOTE_GS4, 500, NOTE_F4, 350, NOTE_A4, 125, 
+	NOTE_C5, 500, NOTE_A4, 375, NOTE_C5, 125, NOTE_E5, 650, REST, 500, NOTE_A5, 500, NOTE_A4, 300, 
+	NOTE_A4, 150, NOTE_A5, 500, NOTE_GS5, 325, NOTE_G5, 175, NOTE_FS5, 125, NOTE_F5, 125, NOTE_FS5, 250, 
+	REST, 325, NOTE_AS, 250, NOTE_DS5, 500, NOTE_D5, 325, NOTE_CS5, 175, NOTE_C5, 125, NOTE_AS4, 125, 
+	NOTE_C5, 250, REST, 350, NOTE_F4, 250, NOTE_GS4, 500, NOTE_F4, 375, NOTE_C5, 125, NOTE_A4, 500, 
+	NOTE_F4, 375, NOTE_C5, 125, NOTE_A4, 650, REST, 650
+};
+
+
+volatile state_t state = STOP;
+
 osMessageQueueId_t ledQ, audioQ, motorQ, connectQ;
 osMessageQueueId_t controlQ;
+osEventFlagsId_t control;
+
+/* Delay Functions */
+
+static void delay(volatile uint32_t nof) {
+  while(nof!=0) {
+    __asm("NOP");
+    nof--;
+  }
+}
+
+//1 nop takes 1 cycle? So 1 delay is 1/48 microsec
+
+/*
+Usage Example:
+delay(0x80000);
+*/
+
+static void delay100x(volatile uint32_t nof) {
+	for(int i =0;i<100;i++) {
+		delay(nof);
+	}
+}
 
 
 /*switch off the LED*/
@@ -70,6 +166,60 @@ void led_control(colours_t colour, ledOnOff on_off) {
  }
 }
 
+/* AUDIO FUNCTIONS */
+
+void playHedwigsTheme() {
+	// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+	// there are two values per note (pitch and duration), so for each note there are four bytes
+	int notes = sizeof(hedwigsThemeMelody) / sizeof(hedwigsThemeMelody[0]);
+
+	// this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+	int wholenote = (60000 * 4) / tempo;
+
+	int divider = 0, noteDuration = 0;
+
+	uint32_t period;
+
+
+		for(int i = 0; i<notes; i+=2) {
+			// calculates the duration of each note
+			divider = hedwigsThemeMelody[i + 1];
+			if (divider > 0) {
+				// regular note, just proceed
+				noteDuration = (wholenote) / divider;
+			} else if (divider < 0) {
+				// dotted notes are represented with negative durations!!
+				noteDuration = (wholenote) / (int)fabs((float)divider);
+				noteDuration *= 1.5; // increases the duration in half for dotted notes
+			}
+			period = TO_MOD(hedwigsThemeMelody[i]);
+			TPM0->MOD = period;
+			TPM0_C0V = period / 8; //12.5% duty cycle
+			osDelay(noteDuration*5/9);
+			TPM0->MOD = 0;
+			TPM0_C0V = 0;
+			osDelay(noteDuration*25/81);
+		}
+}
+
+void playStarWars() {
+	int notes = sizeof(starWarsMelody) / sizeof(starWarsMelody[0]);
+
+	int noteDuration = 0, period = 0;
+		
+		for(int i = 0; i<notes; i+=2) {
+			// calculates the duration of each note
+			noteDuration = starWarsMelody[i + 1];
+			period = TO_MOD(starWarsMelody[i]);
+			TPM0->MOD = period;
+			TPM0_C0V = period / 4; //12.5% duty cycle
+			osDelay(noteDuration*2/3);
+			TPM0->MOD = 0;
+			TPM0_C0V = 0;
+			osDelay(noteDuration*16/25);
+		}
+}
+
 /* IRQ HANDLER FOR UART */
 void UART2_IRQHandler(void) {
 	NVIC_ClearPendingIRQ(UART2_IRQn);
@@ -84,14 +234,7 @@ void UART2_IRQHandler(void) {
 	
 }
 
-/* Delay Function */
 
-static void delay(volatile uint32_t nof) {
-  while(nof!=0) {
-    __asm("NOP");
-    nof--;
-  }
-}
 /* MOTORS */
 /** Stop the motors**/
 void stopMotors(){
@@ -105,55 +248,78 @@ void stopMotors(){
 }
 
 /** Move Reverse **/ 
-void reverse() {
+void reverse(state_t s) {
+	if(state != REVERSE) {
+		TPM1_C1V = 0;
+		TPM2_C1V = 0;
+	}
   TPM1->MOD = 7500;
-	TPM1_C0V = MAX_DUTY_CYCLE/speed;
+	TPM1_C0V = MAX_DUTY_CYCLE;
 	TPM2->MOD = 7500;
-	TPM2_C0V = MAX_DUTY_CYCLE/speed;
+	TPM2_C0V = MAX_DUTY_CYCLE;
 }
 
 /** Move Forward **/
-void forward() {
+void forward(state_t s) {
+	 if(s != FORWARD) {
+		TPM1_C0V = 0;
+		TPM2_C0V = 0;
+	 }
 	 TPM1->MOD = 7500;
-	 TPM1_C1V = MAX_DUTY_CYCLE/speed;
+	 TPM1_C1V = MAX_DUTY_CYCLE;
 	 TPM2->MOD = 7500;
-	 TPM2_C1V = MAX_DUTY_CYCLE/speed;
+	 TPM2_C1V = MAX_DUTY_CYCLE;
 }
 
 /** Rotate Right **/
-void right() {
+void right(state_t s) {
+	 if (s != RIGHT) {
+		  TPM1_C0V = 0;
+		  TPM2_C1V = 0;
+	 }
 	 TPM1->MOD = 7500;
-	 TPM1_C1V = MAX_DUTY_CYCLE/speed;
-	 TPM2->MOD = 0;
-	 TPM2_C1V = 0;
+	 TPM1_C1V = MAX_DUTY_CYCLE/2;
+	 TPM2->MOD = 7500;
+	 TPM2_C0V = MAX_DUTY_CYCLE/2;
 }
 
 /** Rotate Left **/
-void left() {
+void left(state_t s) {
+	 if (s != LEFT) { 
+		 TPM1_C1V = 0;
+		 TPM2_C0V = 0;
+	 }
 	 TPM2->MOD = 7500;
-	 TPM2_C1V = MAX_DUTY_CYCLE/speed;
-	 TPM1->MOD = 0;
-	 TPM1_C1V = 0;
+	 TPM2_C1V = MAX_DUTY_CYCLE/2;
+	 TPM1->MOD = 7500;
+	 TPM1_C0V = MAX_DUTY_CYCLE/2;
 }
+
 /*----------------------------------------------------------------------------
  * Application main thread
  *---------------------------------------------------------------------------*/
-//Need a status flag to prevent continously resetting the MODs
+
+
 void motor_thread () {
 	uint8_t rx_p;
 	for(;;) {
 	osMessageQueueGet(motorQ, &rx_p, NULL, osWaitForever);
-	if(rx_p == FW_MASK){
-			forward();
-		}else if(rx_p == RV_MASK){
-			reverse();
-		}else if(rx_p == RT_MASK){
-			right();
-		}else if(rx_p == LT_MASK){
-			left();
-		}else if(rx_p == STOP_MASK){
+	if(rx_p == STOP_MASK){
 			stopMotors();
-		}	
+			state = STOP;
+		}else if(rx_p == FW_MASK && state != FORWARD){
+			forward(state);
+			state = FORWARD;
+		}else if(rx_p == RV_MASK && state != REVERSE){
+			reverse(state);
+			state = REVERSE;
+		}else if(rx_p == RT_MASK && state != RIGHT){
+			right(state);
+			state = RIGHT;
+		}else if(rx_p == LT_MASK && state != LEFT){
+			left(state);
+			state = LEFT;
+		}
 	}
 }
 
@@ -170,12 +336,29 @@ void  led_rear_thread (void *argument) {
  }
 }
 
+/** 	AUDIO THREAD **/
+void audio_thread() {
+	uint8_t rx_p = 0xFF;
+	
+	for(;;){
+		osMessageQueueGet(audioQ, &rx_p, NULL, 0);
+		if(rx_p == 0x01) {
+			playStarWars();
+			rx_p = 0x00;
+			osMessageQueuePut(audioQ, &rx_p, NULL, 0);
+		} else if(rx_p != 0xFF){
+			playHedwigsTheme();
+		}
+	}
+}
+
 void control_thread(void *argument) {
 	for(;;) {
 		osMessageQueueGet(controlQ, &uart_data, NULL, osWaitForever);
 		osMessageQueuePut(ledQ, &uart_data, NULL, 0); 
 		osMessageQueuePut(motorQ, &uart_data, NULL, 0);
-		osDelay(1000);
+		osMessageQueuePut(audioQ, &uart_data, NULL, 0);
+		//osDelay(1000);
 	}
 }
  
@@ -187,20 +370,25 @@ int main (void) {
 	initUART2(BAUD_RATE);
 	initPWM();
 	initLED();
+	initAudio();
 	//Initialise Queues
 	
-	
-	// audioQ = osMessageQueueNew(MSG_COUNT, sizeof(dataPkt), NULL);
 	// connectQ = osMessageQueueNew(MSG_COUNT, sizeof(dataPkt), NULL);
 	
   osKernelInitialize();                 // Initialize CMSIS-RTOS
-  //led_flag = osEventFlagsNew(NULL);
+	
 	osThreadNew(control_thread, NULL, NULL);
 	controlQ = osMessageQueueNew(MSG_COUNT, sizeof(uint8_t), NULL);
+	
 	osThreadNew(led_rear_thread, NULL, NULL);
 	ledQ = osMessageQueueNew(MSG_COUNT, sizeof(uint8_t), NULL);
-	osThreadNew (motor_thread, NULL, NULL);
+	
+	osThreadNew(motor_thread, NULL, NULL);
 	motorQ = osMessageQueueNew(MSG_COUNT, sizeof(uint8_t), NULL);
+	
+	osThreadNew(audio_thread, NULL, NULL);
+	audioQ = osMessageQueueNew(MSG_COUNT, sizeof(uint8_t), NULL);
+	
   osKernelStart();                      // Start thread execution
   for (;;) {}
 }
